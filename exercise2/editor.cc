49b9c21fb6bbbeb93f33c73db0d7a5a79c74ae98
Simon Tenggren <simon.tenggren@gmail.com>
2020-02-07 14:32:32 +0100

    finished editor.cc

--- exercise2/editor.cc
+++ exercise2/editor.cc
@@ -1,9 +1,40 @@
 #include "editor.h"
-
+#include <iostream>
 #include <string>
 
 using std::string;
-
+using std::cout;
+using std::endl;
 string::size_type Editor::find_left_par(string::size_type pos) const {
-	return 0;
+    char parType = this->text[pos];
+    char find;
+    switch (parType) {
+        case ']':
+            find = '[';
+            break;
+        case ')':
+            find = '(';
+            break;
+        case '}':
+            find = '{';
+            break;
+        default:
+            return string::npos;
+    }
+    int ignore = 0;
+    int position = static_cast<int> (pos) - 1;
+    while ( position >= 0 ){
+        char curr = this->text[position];
+        if (curr == find){
+            if (ignore <= 0){
+                return position;
+            } else {
+                --ignore;
+            }
+        } else if (curr == parType){
+            ++ignore;
+        }
+        --position;
+    }
+    return string::npos;
 }
