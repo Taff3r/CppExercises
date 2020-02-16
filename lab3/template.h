9e615c3ddc7b6bcdf2dc2de9af694f99fb56eb1f
simon <simon.tenggren@gmail.com>
2020-02-16 21:20:12 +0100

    added templates

--- lab3/template.h
+++ lab3/template.h
@@ -4,11 +4,27 @@
 #include <string>
 #include <sstream>
 #include "date.h"
+
 template <typename T>
 std::string toString(T &t){
     std::stringstream s;
     s << t;
     return s.str();
+}
+
+std::string toString(Date &t){
+    std::stringstream s;
+    s << t;
+    return "The date is: " + s.str();
+}
+
+template <typename R>
+R string_cast(const std::string &s){
+    std::stringstream ss;
+    R r;
+    ss << s;
+    ss >> r; 
+    return r;
 }
 
 #endif
